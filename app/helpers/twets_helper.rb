886929575665190f1eebebc50c8e074201cbb0e4
wallyali <abdishwak@gmail.com>
2014-02-10 22:13:00 -0600

    Add modified twets helper

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,2 +1,15 @@
 module TwetsHelper
+	def create_link(twet)
+		modified_twet = twet.content.split('')
+		modified_twet.each do |twet|
+			twet.gsub!(/@.*/) do |match|
+				match = [1..-1]
+				link_to "@#{match}", match
+			end
+		end
+		"#{modified_twet.join(' ')}"
+	end
 end
+
+
+
