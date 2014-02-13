851dc7d08e4fcd43664194816d67eaa93bd2114a
wallyali <abdishwak@gmail.com>
2014-02-12 22:43:09 -0600

    add formated index view

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,11 +1,11 @@
 module TwetsHelper
 
 	def create_link(twet)
-		modified_twet = twet.content.split('')
+		modified_twet = twet.content.split
 		modified_twet.each do |twet|
 			twet.gsub!(/@.*/) do |match|
 				match = [1..-1]
-				link_to "@#{match}", match
+				self.class.helpers.link_to "@#{match}", match
 			end
 		end
 		"#{modified_twet.join('')}"
